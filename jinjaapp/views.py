97c1dad1b3862d7f522b2f1b7ef2ab51d4547cb5
Leaders2022 <georgeobby@gmail.com>
2024-07-11 12:52:40 +0300

    initial commit

---
+++ jinjaapp/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+
+def base(request):
+    return render(request, 'base.html')
+
+
+def index(request):
+    return render(request, 'home.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def services(request):
+    return render(request, 'services.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
